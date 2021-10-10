77fb523c3452d4700fcc0d21b033fc30319f7d4c
psu <pete.su@gmail.com>
2021-10-10 12:51:17 -0400

    more 12

---
+++ 1612.09375-12/update-all.sh
@@ -0,0 +1,25 @@
+pdflatex BasicCategoryTheory.tex
+pdflatex BasicCategoryTheory.tex
+pdflatex BasicCategoryTheory.tex
+
+pdflatex BasicCategoryTheory-fancy.tex
+pdflatex BasicCategoryTheory-fancy.tex
+pdflatex BasicCategoryTheory-fancy.tex
+
+pdflatex BasicCategoryTheory-Charter.tex
+pdflatex BasicCategoryTheory-Charter.tex
+pdflatex BasicCategoryTheory-Charter.tex
+
+pdflatex BasicCategoryTheory-Charter-euler.tex
+pdflatex BasicCategoryTheory-Charter-euler.tex
+pdflatex BasicCategoryTheory-Charter-euler.tex
+
+cp BasicCategoryTheory.pdf "/Users/psu/Dropbox/GoodReader/Math and Physics/More Math/Cats/Leinster  - Basic Category Theory (2016).pdf"
+cp BasicCategoryTheory-Charter.pdf "/Users/psu/Dropbox/GoodReader/Math and Physics/More Math/Cats/Leinster  - Basic Category Theory (2016,ch).pdf" 
+cp BasicCategoryTheory-Charter-euler.pdf "/Users/psu/Dropbox/GoodReader/Math and Physics/More Math/Cats/Leinster  - Basic Category Theory (2016,ch,eu).pdf"
+cp BasicCategoryTheory-fancy.pdf "/Users/psu/Dropbox/GoodReader/Math and Physics/More Math/Cats/Leinster  - Basic Category Theory (2016,fancy).pdf"
+
+cp BasicCategoryTheory.pdf "/Users/psu/Dropbox/GoodReader/Math and Physics/Types, etc/Leinster  - Basic Category Theory (2016).pdf"
+cp BasicCategoryTheory-Charter.pdf "/Users/psu/Dropbox/GoodReader/Math and Physics/Types, etc/Leinster  - Basic Category Theory (2016,ch).pdf" 
+cp BasicCategoryTheory-Charter-euler.pdf "/Users/psu/Dropbox/GoodReader/Math and Physics/Types, etc/Leinster  - Basic Category Theory (2016,ch,eu).pdf"
+cp BasicCategoryTheory-fancy.pdf "/Users/psu/Dropbox/GoodReader/Math and Physics/Types, etc/Leinster  - Basic Category Theory (2016,fancy).pdf"
