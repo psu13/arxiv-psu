fb9b78584073c33d228695fb619999c3c1e30f38
psu <psu_13@mac.com>
2023-08-27 08:20:05 -0400

    a2.py update

--- scripts/a2.py
+++ scripts/a2.py
@@ -11,12 +11,25 @@ search = arxiv.Search(
   sort_by = arxiv.SortCriterion.SubmittedDate
 )
 
+doi = "nope"
+
 for result in search.results():
   for link in result.links:
     if link.title == 'doi':
       doi = link.href
   arxiv = result.entry_id
 
-cmd = "d2a " + '"' + doi + '"' + " " + arxiv
-
-os.system(cmd)
+if doi == "nope":
+  for result in search.results():
+    print("@article{arxiv_article,")
+    names = map((lambda author:author.name), result.authors)
+    print("\t author = {"+' and '.join(names)+"},")
+    print("\t title = {{"+result.title+"}},")
+    print("\t journal = {arXiv preprint},")
+    print("\t year = {", result.published.year , "},")
+    link = result.links[0]
+    print("\t note = {\\href{" + link.href + "}{arxiv:" + the_id + "}}")
+    print("}")
+else:
+  cmd = "d2a " + '"' + doi + '"' + " " + arxiv
+  os.system(cmd)
